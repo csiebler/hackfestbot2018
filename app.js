8ecc27e95b51e860eefd6feaa84a55117f48e052
Clemens Siebler <clemens.siebler@gmail.com>
2018-07-05 10:17:26 +0200

    Added LUIS and other stuff

--- app.js
+++ app.js
@@ -3,32 +3,68 @@ var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
 require('dotenv').config();
 
-const STORAGE_CONNECTION = process.env['STORAGE_CONNECTION'];
+const STORAGE_CONNECTION = process.env.STORAGE_CONNECTION;
+const LUIS_MODEL_URL = process.env.LUIS_MODEL_URL;
 
-// Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
-// Create chat connector for communicating with the Bot Framework Service
+
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
     openIdMetadata: process.env.BotOpenIdMetadata
 });
 
-// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
 var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, STORAGE_CONNECTION);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
-// Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
+var recognizer = new builder.LuisRecognizer(LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                bot.beginDialog(message.address, '/')
+            }
+        });
+    }
+});
+
 bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+    session.send('Welcome to BIBO!');
+    session.send('You can ask me things like "show me this year\'s sales numbers\'');
+}).triggerAction({
+    matches: 'Greeting'
+});
+
+bot.dialog('Show-KPIs', [
+    function (session) {
+        session.send('PLACEHOLDER - I will return something useful here');
+        var msg = new builder.Message(session);
+        msg.attachmentLayout(builder.AttachmentLayout.carousel)
+        msg.attachments([
+            new builder.HeroCard(session)
+                .title("Your numbers")
+                .images([builder.CardImage.create(session, 'https://oxcrx34285.i.lithium.com/t5/image/serverpage/image-id/18454iF5B3541309A64563/image-size/large?v=1.0&px=600')])
+        ]);
+        session.send(msg);
+        session.endDialog();
+    }
+]).triggerAction({
+    matches: 'show-kpis'
+});
+
+bot.dialog('help', function (session) {
+    session.endDialog("You can ask me things like<br>'Show me the sales numbers for this quarter'<br>'how many items did we sell in Germany?'");
+}).triggerAction({
+    matches: 'Help'
 });
